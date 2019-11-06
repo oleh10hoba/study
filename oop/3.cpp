fae3fafe517d7a8c13da7853e20ad1eee602a20c
oleh10hoba <56839343+oleh10hoba@users.noreply.github.com>
2019-11-06 13:39:06 +0100

    Update 3.cpp

--- oop/3.cpp
+++ oop/3.cpp
@@ -6,57 +6,85 @@ class Table2D
     int width;
     int height;
     int *begin;
+    int *array_1;
 public:
     Table2D(string name, int width, int height):
-        name(name), width(width), height(height) 
+        name(name), width(width), height(height)
     {
-        cout << "Constructor " << name << endl; 
-        int *array_1 = new int[width]; 
+        cout << "Constructor " << name << endl;
+        array_1 = new int[width*height];
     }
     Table2D(const Table2D& copied_object) :
         name(copied_object.name), width(copied_object.width), height(copied_object.height)
     {
-        cout << "Constructor copid " << name << " to " << copied_object.name << endl; 
+        array_1 = new int[width*height];
+        for(int i = 0; i < sizeof(array_1); i++)
+        {
+            array_1[i] = copied_object.array_1[i];
+        }
+        cout << "Constructor copid " << name << " to " << copied_object.name << endl;
     }
-    const add(const Table2D& added)
-    {
-        
-    }
+
     void set_name(string name)
     {
         this->name = name;
     }
-    string get_name()
+    void show_ellement(int row, int collumn)
+    {
+        cout << array_1[row*width+collumn];
+    }
+    string get_name() const
     {
         return name;
     }
-    void set_element(int row, int column, int value)
+    void set_element(int row, int collumn, int value)
     {
-        array_1[row][column] = value;
+        array_1[row*width+collumn] = value;
     }
-    int get_width()
+    int get_width() const
     {
         return width;
     }
-    int get_height()
+    int get_height() const
     {
         return height;
     }
-    void show()
+    void show()const
     {
         cout << name << " " << width << " " << height << endl;
+    }
+    Table2D add(const Table2D& added)
+    {
+        Table2D new_object = Table2D("Added",width,height);
+        for(int i = 0; i < sizeof(new_object.array_1); i++)
+        {
+            new_object.array_1[i] = added.array_1[i] + this->array_1[i];
+        }
+        return new_object;
+    }
+    Table2D multiply(const Table2D& multipled)
+    {
+        Table2D new_object = Table2D("Multipled",width,height);
+        for(int i = 0; i < sizeof(new_object.array_1); i++)
+        {
+            new_object.array_1[i] = multipled.array_1[i] * this->array_1[i];
+        }
+        return new_object;
     }
     ~Table2D()
     {
         cout << "Destroyer " << name << endl;
-        delete array_1[];
+        //delete array_1[];
     }
 };
 int main()
 {
-    Table2D t1 = Table2D("tab", 77, 110);
+    Table2D t1 = Table2D("t1", 3, 4);
     Table2D t2(t1);
+    Table2D t3 = Table2D("t3", 5, 6);
+    t2.set_name("t2");
     t1.show();
     t2.show();
+    t3.add(t3);
     return 0;
 }
